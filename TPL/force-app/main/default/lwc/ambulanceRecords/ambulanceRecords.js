d85e1fb343867c4436f503d1ddc008e1e1ce9f1f
Anudish Jinturkar <anudish.jinturkar@cgi.com>
2023-01-30 11:53:24 -0800

    Updates containing UI Lookup

--- TPL/force-app/main/default/lwc/ambulanceRecords/ambulanceRecords.js
+++ TPL/force-app/main/default/lwc/ambulanceRecords/ambulanceRecords.js
@@ -3,36 +3,67 @@ import getHealthcareCostsAmbulanceForAccount from '@salesforce/apex/HCCCostContr
 import { updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import CASE_FIELD from '@salesforce/schema/Healthcare_Cost__c.Case__c';
+import ACCOUNT_FIELD from '@salesforce/schema/Healthcare_Cost__c.Account__c';
 import COST_INCLUDE_FIELD from '@salesforce/schema/Healthcare_Cost__c.Cost_Include__c';
 import COST_REVIEW_FIELD from '@salesforce/schema/Healthcare_Cost__c.Cost_Review__c';
 import COST_FIELD from '@salesforce/schema/Healthcare_Cost__c.Cost__c';
 import BASIC_AMOUNT_FIELD from '@salesforce/schema/Healthcare_Cost__c.Basic_Amount__c';
 import TOTAL_OVERRIDE_FIELD from '@salesforce/schema/Healthcare_Cost__c.Total_Override__c';
-import getCountofAmbulanceRecords from '@salesforce/apex/HCCCostController.getCountofAmbulanceRecords';
-
+import DATE_OF_SERVICE_FIELD from '@salesforce/schema/Healthcare_Cost__c.Date_of_Service__c';
+import LOCATION_RESPONDED_FIELD from '@salesforce/schema/Healthcare_Cost__c.Location_Responded__c';
+import relatedCaseRecords from '@salesforce/apex/HCCCostController.caseListIndividual';
+import relatedAccountRecords from '@salesforce/apex/HCCCostController.listOfIndividualAccounts';
+import CaseNumber from '@salesforce/schema/Case.CaseNumber';
 const COLS = [
     {
-        label: 'Case Name',
+        label: 'Case Number',
         fieldName: CASE_FIELD.fieldApiName,
         type: 'lookup',
-        editable: true
+        typeAttributes: {
+            placeholder: 'Choose Case',
+            object: 'Healthcare_Cost__c',
+            fieldName: CASE_FIELD.fieldApiName,
+            label: 'CaseNumber',
+            value: { fieldName: CASE_FIELD.fieldApiName},
+            context:{fieldName: 'Id'},
+            variant: 'label-hidden',
+            name: 'Case',
+            fields: ['Case.CaseNumber'],
+            target: '_self'
+        },
+        editable: true,
+        cellAttributes:{
+            class: { fieldName: 'CaseNumberClass'}
+        }
     },
     {
         label: 'Cost Include',
         fieldName: COST_INCLUDE_FIELD.fieldApiName,
         type:'boolean',
-        editable: false
+        editable: true
     },
     {
         label: 'Cost Review',
         fieldName: COST_REVIEW_FIELD.fieldApiName,
         type:'boolean',
-        editable:false
+        editable:true
+    },
+    {
+        label: 'Date of Service',
+        fieldName: DATE_OF_SERVICE_FIELD.fieldApiName,
+        type: 'date',
+        editable: false
     },
     {
         label: 'Basic Amount',
         fieldName: BASIC_AMOUNT_FIELD.fieldApiName,
         type: 'currency',
+        editable: false
+    },
+    {
+        label: 'Location Responded',
+        fieldName: LOCATION_RESPONDED_FIELD.fieldApiName,
+        type: 'text',
         editable: false
     },
     {
@@ -54,56 +85,87 @@ export default class AmbulanceRecords extends LightningElement {
     column = COLS;
     isFirstPage = true;
     isLastPage = false;
-     pageNumber = 1;
-    totalPage = 0;
-    @api recordSize = 5;
-    
+    totalRecords = 0; //Total no.of records
+    totalPages; //Total no.of pages
+    pageNumber = 1; //Page number
+    pageSizeOptions = [5, 10, 25, 50, 75, 100, 150, 200, 500]; //Page size options
+    records = []; //All records available in the data table
+    pageSize; //No.of records to be displayed per page
+    recordsToDisplay = []; //Records to be displayed on the page
+
+    renderedCallback() {
+        if (!this.isComponentLoaded) {
+            /* Add Click event listener to listen to window click to reset the lookup selection 
+            to text view if context is out of sync*/
+            window.addEventListener('click', (evt) => {
+                this.handleWindowOnclick(evt);
+            });
+            this.isComponentLoaded = true;
+        }
+    }
+
     @wire(getHealthcareCostsAmbulanceForAccount, { accId: '$recordId' })
-    healthcareCostsAmbulanceForAccount;   
-
-    @wire(getCountofAmbulanceRecords,{ accId: '$recordId'})
-    countOfAmbulanceRecords;
-
-    handlePageLoad(){
-        this.totalRecords = countOfAmbulanceRecords
-        this.recordSize = Number(this.recordSize)
-        this.totalPage = Math.ceil(this.totalRecords/this.recordSize)
-
-    }
-
-    get disablePrevious(){ 
-        return this.currentPage<=1
-    }
-
-    get disableNext(){ 
-        return this.currentPage>=this.totalPage
-    }
-
-    previousHandler(){ 
-        if(this.currentPage>1){
-            this.currentPage = this.currentPage-1
-            this.updateRecords()
+    healthcareCostsAmbulanceForAccount({error,data}){
+        if(data != null && data){
+            console.log('Data of Ambulance Records --> ' + JSON.stringify(data));
+            this.records = data;
+            this.totalRecords = data.length;
+            this.pageSize = this.pageSizeOptions[0]; 
+            this.paginationHelper(); // call helper menthod to update pagination logic
+        }
+        else if(error){
+            console.error(error);
         }
     }
 
-    nextHandler(){
-        if(this.currentPage < this.totalPage){
-            this.currentPage = this.currentPage+1
-            this.updateRecords()
-        }
+    get bDisableFirst() {
+        return this.pageNumber == 1;
+    }
+    get bDisableLast() {
+        return this.pageNumber == this.totalPages;
+    }
+    
+    handleRecordsPerPage(event) {
+        this.pageSize = event.target.value;
+        this.paginationHelper();
+    }
+    previousPage() {
+        this.pageNumber = this.pageNumber - 1;
+        this.paginationHelper();
+    }
+    nextPage() {
+        this.pageNumber = this.pageNumber + 1;
+        this.paginationHelper();
+    }
+    firstPage() {
+        this.pageNumber = 1;
+        this.paginationHelper();
+    }
+    lastPage() {
+        this.pageNumber = this.totalPages;
+        this.paginationHelper();
     }
 
-    updateRecords(){ 
-        const start = (this.currentPage-1)*this.recordSize
-        const end = this.recordSize*this.currentPage
-        this.visibleRecords = this.totalRecords.slice(start, end)
-        this.dispatchEvent(new CustomEvent('update',{ 
-            detail:{ 
-                records:this.visibleRecords
+        // JS function to handel pagination logic 
+        paginationHelper() {
+            this.recordsToDisplay = [];
+            // calculate total pages
+            this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+            // set page number 
+            if (this.pageNumber <= 1) {
+                this.pageNumber = 1;
+            } else if (this.pageNumber >= this.totalPages) {
+                this.pageNumber = this.totalPages;
             }
-        }))
-    }
-
+            // set records to display on current page 
+            for (let i = (this.pageNumber - 1) * this.pageSize; i < this.pageNumber * this.pageSize; i++) {
+                if (i === this.totalRecords) {
+                    break;
+                }
+                this.recordsToDisplay.push(this.records[i]);
+            }
+        }
+    
     async handleSave(event){
         
         // Convert datatable draft values into record objects
