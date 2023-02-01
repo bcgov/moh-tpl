3ad5c7b582042af5de06b1185266e81ff2b34385
Anudish Jinturkar <anudish.jinturkar@cgi.com>
2023-02-01 12:27:09 -0800

    Revert case LWC remove account lookup

--- TPL/force-app/main/default/lwc/ambulanceRecordsCase/ambulanceRecordsCase.js
+++ TPL/force-app/main/default/lwc/ambulanceRecordsCase/ambulanceRecordsCase.js
@@ -4,7 +4,7 @@ import { LightningElement, wire, api } from 'lwc';
 import { updateRecord } from 'lightning/uiRecordApi';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import ACCOUNT_FIELD from '@salesforce/schema/Healthcare_Cost__c.Account__c';
+import HCCOST_FIELD from '@salesforce/schema/Healthcare_Cost__c.Name';
 import COST_INCLUDE_FIELD from '@salesforce/schema/Healthcare_Cost__c.Cost_Include__c';
 import COST_REVIEW_FIELD from '@salesforce/schema/Healthcare_Cost__c.Cost_Review__c';
 import COST_FIELD from '@salesforce/schema/Healthcare_Cost__c.Cost__c';
@@ -13,38 +13,12 @@ import TOTAL_OVERRIDE_FIELD from '@salesforce/schema/Healthcare_Cost__c.Total_Ov
 import DATE_OF_SERVICE_FIELD from '@salesforce/schema/Healthcare_Cost__c.Date_of_Service__c';
 import LOCATION_RESPONDED_FIELD from '@salesforce/schema/Healthcare_Cost__c.Location_Responded__c';
 import getHealthcareCostsAmbulanceForCase from '@salesforce/apex/HCCCostController.getHealthcareCostsAmbulanceForCase';
-import saveDraftValues from '@salesforce/apex/HCCCostController.saveDraftValues';
+
 
 const COLUMNS = [
     {
         label: 'HealthCare Cost Name',
-        fieldName: 'linkName',
-        type: 'url',
-        typeAttributes: {
-            label: { fieldName: 'Name' },
-            target: '_self'
-        }
-    },
-    {
-        label: 'Account Name',
-        fieldName: 'Account__c',
-        type: 'lookup',
-        typeAttributes: {
-            placeholder: 'Choose Account',
-            object: 'Healthcare_Cost__c',
-            fieldName: 'Account__c',
-            label: 'Account',
-            value: { fieldName: 'Account__c'},
-            context: { fieldName: 'Id' },
-            variant: 'label-hidden',
-            name: 'Account',
-            fields: ['Account.Name'],
-            target: '_self'
-        },
-        
-        cellAttributes: {
-            class: { fieldName: 'accountNameClass' }
-        }
+        fieldName: HCCOST_FIELD.fieldApiName,
     },
     {
         label: 'Cost Include',
@@ -103,55 +77,23 @@ export default class AmbulanceRecordsCase extends LightningElement {
     recordsToDisplay = []; //Records to be displayed on the page
     lastSavedData;
     error;
-    wiredRecords;
+
     showSpinner = false;
-    draftValues = [];
-    privateChildren = {}; //used to get the datatable lookup as private childern of customDatatable
+   
 
-    renderedCallback() {
-        if (!this.isComponentLoaded) {
-            /* Add Click event listener to listen to window click to reset the lookup selection 
-            to text view if context is out of sync*/
-            window.addEventListener('click', (evt) => {
-                this.handleWindowOnclick(evt);
-            });
-            this.isComponentLoaded = true;
-        }
-    }
-
-    disconnectedCallback() {
-        window.removeEventListener('click', () => { });
-    }
-
-    handleWindowOnclick(context) {
-        this.resetPopups('c-datatable-lookup', context);
-    }
-
-    //create object value of datatable lookup markup to allow to call callback function with window click event listener
-    resetPopups(markup, context) {
-        let elementMarkup = this.privateChildren[markup];
-        if (elementMarkup) {
-            Object.values(elementMarkup).forEach((element) => {
-                element.callbacks.reset(context);
-            });
-        }
-    }
-
-    @wire (getHealthcareCostsAmbulanceForCase,{caseId: '$recordId'})
-    wiredHealthcareCostsAmbulanceForCase(result){
-        this.wiredRecords = result;
-        const { data, error } = result;
-        if (data) {
-            this.records = JSON.parse(JSON.stringify(data));
-            this.records.forEach(record => {
-                record.linkName = '/' + record.Id;
-                record.accountNameClass = 'slds-cell-edit';
-            });
+    @wire(getHealthcareCostsAmbulanceForCase, { caseId: '$recordId' })
+    healthcareCostsAmbulanceForCase({error,data}){
+        if(data != null && data){
+            console.log('Data of Ambulance Records --> ' + JSON.stringify(data));
+            this.records = data;
             this.totalRecords = data.length;
             this.pageSize = this.pageSizeOptions[0]; 
             this.paginationHelper(); // call helper menthod to update pagination logic
-            this.error = undefined;
-        } else if (error) {
+        }
+        else if(error){
+            console.error(error);
+        }
+        else if (error) {
             this.records = undefined;
             this.error = error;
         } else {
@@ -160,122 +102,6 @@ export default class AmbulanceRecordsCase extends LightningElement {
         }
         this.lastSavedData = this.records;
         this.showSpinner = false;
-    }
-
-     // Event to register the datatable lookup mark up.
-     handleItemRegister(event) {
-        event.stopPropagation(); //stops the window click to propagate to allow to register of markup.
-        const item = event.detail;
-        if (!this.privateChildren.hasOwnProperty(item.name))
-            this.privateChildren[item.name] = {};
-        this.privateChildren[item.name][item.guid] = item;
-    }
-
-    handleChange(event) {
-        event.preventDefault();
-        this.Account__c = event.target.value;
-        this.showSpinner = true;
-        console.log('Inside Handle Change ');
-    }
-
-    handleCancel(event) {
-        event.preventDefault();
-        this.records = JSON.parse(JSON.stringify(this.lastSavedData));
-        this.handleWindowOnclick('reset');
-        this.draftValues = [];
-    }
-
-    handleCellChange(event) {
-        event.preventDefault();
-        this.updateDraftValues(event.detail.draftValues[0]);
-        console.log(' Handle Cell Change');
-    }
-
-    //Captures the changed lookup value and updates the records list variable.
-    handleValueChange(event) {
-        event.stopPropagation();
-        let dataRecieved = event.detail.data;
-        let updatedItem;
-        switch (dataRecieved.label) {
-            case 'Account':
-                updatedItem = {
-                    Id: dataRecieved.context,
-                    Account__c: dataRecieved.value
-                };
-                // Set the cell edit class to edited to mark it as value changed.
-                this.setClassesOnData(
-                    dataRecieved.context,
-                    'accountNameClass',
-                    'slds-cell-edit slds-is-edited'
-                );
-                break;
-            default:
-                this.setClassesOnData(dataRecieved.context, '', '');
-                break;
-        }
-        this.updateDraftValues(updatedItem);
-        this.updateDataValues(updatedItem);
-        console.log('Handle Value Change');
-    }
-
-    updateDataValues(updateItem) {
-        let copyData = JSON.parse(JSON.stringify(this.records));
-        copyData.forEach((item) => {
-            if (item.Id === updateItem.Id) {
-                for (let field in updateItem) {
-                    item[field] = updateItem[field];
-                }
-            }
-        });
-        this.records = [...copyData];
-        console.log('Updated data values log');
-    }
-
-    updateDraftValues(updateItem) {
-        let draftValueChanged = false;
-        let copyDraftValues = JSON.parse(JSON.stringify(this.draftValues));
-        copyDraftValues.forEach((item) => {
-            if (item.Id === updateItem.Id) {
-                for (let field in updateItem) {
-                    item[field] = updateItem[field];
-                }
-                draftValueChanged = true;
-            }
-        });
-        if (draftValueChanged) {
-            this.draftValues = [...copyDraftValues];
-        } else {
-            this.draftValues = [...copyDraftValues, updateItem];
-        }
-        console.log('Update Draft values');
-    }
-
-    handleEdit(event) {
-        event.preventDefault();
-        let dataRecieved = event.detail.data;
-        this.handleWindowOnclick(dataRecieved.context);
-        switch (dataRecieved.label) {
-            case 'Account':
-                this.setClassesOnData(
-                    dataRecieved.context,
-                    'accountNameClass',
-                    'slds-cell-edit'
-                );
-                break;
-            default:
-                this.setClassesOnData(dataRecieved.context, '', '');
-                break;
-        };
-        console.log('Inside handle edit method');
-    }
-
-    setClassesOnData(id, fieldName, fieldValue) {
-        this.records = JSON.parse(JSON.stringify(this.records));
-        this.records.forEach((detail) => {
-            if (detail.Id === id) {
-                detail[fieldName] = fieldValue;
-            }
-        });
     }
 
     get bDisableFirst() {
@@ -326,36 +152,46 @@ export default class AmbulanceRecordsCase extends LightningElement {
         }
     }
 
-    handleSave(event) {
-        event.preventDefault();
+    async handleSave(event){
         this.showSpinner = true;
-        console.log('Check values of draft : ');
-        console.log(this.draftValues);
-        // Update the draftvalues
-        saveDraftValues({ data: this.draftValues })
-            .then(() => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'HC Cost updated successfully',
-                        variant: 'success'
-                    })
-                );
-                console.log('HandleSave method');
-                //Get the updated list with refreshApex.
-                refreshApex(this.wiredRecords).then(() => {
-                    this.records.forEach(record => {
-                        record.accountNameClass = 'slds-cell-edit';
-                    });
-                    this.draftValues = [];
-                    console.log('Inline account update');
-                });
-                
-            })
-            .catch(error => {
-                console.log('error : ' + JSON.stringify(error));
-                this.showSpinner = false;
-           });
+        // Convert datatable draft values into record objects
+        const records = event.detail.draftValues.slice().map((draftValue) => {
+            const fields = Object.assign({}, draftValue);
+            return { fields };
+        });
+
+        // Clear all datatable draft values
+        this.draftValues = [];
+
+        try {
+            // Update all records in parallel thanks to the UI API
+            const recordUpdatePromises = records.map((record) =>
+                updateRecord(record)
+            );
+            await Promise.all(recordUpdatePromises);
+
+            // Report success with a toast
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Healthcare Costs Ambulance updated',
+                    variant: 'success'
+                })
+            );
+
+            // Display fresh data in the datatable
+            await refreshApex(this.healthcareCostsAmbulance);
+            this.showSpinner = false;
+        } catch (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error updating or reloading Healthcare Costs',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+            this.showSpinner = false;
+        }
     }
     
 }
